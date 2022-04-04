0387a92980130a9e9bee11a3580bbe3d9cc29b31
BradSlavens <bradslavens@gmail.com>
2022-04-04 09:42:56 -0700

    tried js add class for animation did not work with interval

--- myJs.js
+++ myJs.js
@@ -1,2 +1,5 @@
-const carouselImages = document.querySelectorAll("#carousel > li")
-console.log(carouselImages)
+const intervalId = setInterval(() => {
+    const carousel = document.getElementById("carousel")
+    carousel.classList.add("move")    
+    console.log("moving")
+}, 1000);
